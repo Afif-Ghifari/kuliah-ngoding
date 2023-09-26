fc6fa50465fb296b405b68abb104163e35f0bcee
Afif-Ghifari <aghifari1204@gmail.com>
2023-09-27 06:56:36 +0700

    pemilihan percobaan 1

---
+++ semester1/jobsheet5/PemilihanPercobaan117.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan117 {
+
+    public static void main(String[] args) {
+        
+        // deklarasi dan input
+        Scanner input18 = new Scanner(System.in);
+        String hasil;
+
+        // input nilai
+        System.out.print("Masukkan angka : ");
+        int angka = input18.nextInt();
+         
+        // proses
+        hasil = (angka % 2 == 0) ? "genap" : "ganjil";
+        
+        // output
+        System.out.println("Ternary");
+        System.out.println(angka+" adalah "+hasil);
+    }
+}
