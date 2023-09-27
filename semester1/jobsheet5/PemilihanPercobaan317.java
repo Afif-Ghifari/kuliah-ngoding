c77009e94b802043c241c740b8d1e9dcb98421eb
Afif-Ghifari <aghifari1204@gmail.com>
2023-09-27 09:25:05 +0700

    Modifikasi pemilihan 3

--- semester1/jobsheet5/PemilihanPercobaan317.java
+++ semester1/jobsheet5/PemilihanPercobaan317.java
@@ -22,7 +22,7 @@ public class PemilihanPercobaan317 {
             case '+':
                 hasil = angka1 + angka2;
                 System.out.println(angka1 + " + " + angka2 + " = " + hasil);
-                break;
+                
             case '-':
                 hasil = angka1 - angka2;
                 System.out.println(angka1 + " - " + angka2 + " = " + hasil);
