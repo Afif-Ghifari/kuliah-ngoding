e007800a6c17486488a571ce65b1f7ef41ee8d82
Afif-Ghifari <aghifari1204@gmail.com>
2023-10-04 08:56:03 +0700

    modifikasi Pemilihan2Percobaan217

--- semester1/jobsheet6/Pemilihan2Percobaan217.java
+++ semester1/jobsheet6/Pemilihan2Percobaan217.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 public class Pemilihan2Percobaan217 {
     public static void main(String[] args) {
         
+        // Deklarasi variabel
         Scanner input17 = new Scanner(System.in);
         float sudut1, sudut2, sudut3, totalSudut;
 
+        // Input nilai
         System.out.print("Masukkan sudut 1: ");
         sudut1 = input17.nextFloat();
         System.out.print("Masukkan sudut 2: ");
@@ -12,13 +14,23 @@ public class Pemilihan2Percobaan217 {
         System.out.print("Masukkan sudut 3: ");
         sudut3 = input17.nextFloat();
 
+        // Proses
         totalSudut = sudut1 + sudut2 + sudut3;
 
+        // Pemilihan
         if (totalSudut == 180) {
             if ((sudut1 == 90)||(sudut2 == 90)||(sudut3 == 90)) {
                 System.out.println("Segitiga tersebut adalah segitiga siku-siku");
             } else {
-                System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
+                if (sudut1 == sudut2 && sudut2 == sudut3 && sudut1 == sudut3 ) {
+                    System.out.println("Segitiga tersebut adalah segitiga sama sisi");
+                } else {
+                    if (sudut1 == sudut2 || sudut2 == sudut3 || sudut1 == sudut3) {
+                        System.out.println("Segitiga tersebut adalah segitiga sama kaki");
+                    } else {
+                        System.out.println("Segitiga tersebut adalah segitiga sembarang");
+                    }
+                }
             }
         } else {
             System.out.println("Bukan segitiga");
