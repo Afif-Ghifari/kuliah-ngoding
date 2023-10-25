0d98135615e6c7c99cda256dab6af7f4a4ea85a5
Afif-Ghifari <aghifari1204@gmail.com>
2023-10-25 07:17:59 +0700

    ArrayBilangan17

---
+++ semester1/jobsheet9/ArrayBilangan17.java
@@ -0,0 +1,11 @@
+public class ArrayBilangan17 {
+    public static void main(String[] args) {
+        // deklarasi variabel
+        int[] bil = {5, 13, -7, 17};
+        
+        // output nilai
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
+    }
+}
