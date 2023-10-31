259c6769a33178e87c254bacbb2810bc3d98a85c
Afif-Ghifari <aghifari1204@gmail.com>
2023-10-31 20:52:57 +0700

    Bioskop17.java pertanyaan 11

--- semester1/jobsheet10/Bioskop17.java
+++ semester1/jobsheet10/Bioskop17.java
@@ -17,8 +17,8 @@ public class Bioskop17 {
         // Output program
         System.out.println("Penonton pada baris ke-3: ");
 
-        for (int i = 0; i < penonton[2].length; i++) {
-            System.out.println(penonton[2][i]);
+        for (String i : penonton[2]) {
+            System.out.println(i);
         }
     }
 }
