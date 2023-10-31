e686a3bb69784556685079a5901ffbd34591bbdb
Afif-Ghifari <aghifari1204@gmail.com>
2023-10-31 19:46:21 +0700

    Bioskop17.java

---
+++ semester1/jobsheet10/Bioskop17.java
@@ -0,0 +1,24 @@
+public class Bioskop17 {
+    public static void main(String[] args) {
+
+        // Deklarasi variabel
+        String[][] penonton = new String[4][2];
+
+        // Input nilai
+        penonton[0][0] = "Amin";
+        penonton[0][1] = "Bena";
+        penonton[1][0] = "Candra";
+        penonton[1][1] = "Dela";
+        penonton[2][0] = "Eka";
+        penonton[2][1] = "Farhan";
+        penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
+        
+        
+        // Output program
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.printf("%s \t %s \n", penonton[i][0], penonton[i][1]);
+            
+        }
+    }
+}
