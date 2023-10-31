f97598333426e3f152159698e48df560713b3612
Afif-Ghifari <aghifari1204@gmail.com>
2023-10-31 20:09:42 +0700

    Bioskop17.java output foreach

--- semester1/jobsheet10/Bioskop17.java
+++ semester1/jobsheet10/Bioskop17.java
@@ -17,9 +17,8 @@ public class Bioskop17 {
         // Output program
         System.out.println(penonton.length);
 
-        for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke-"+(i+1)+" : " + penonton[i].length);
-            
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: "+barisPenonton.length);
         }
     }
 }
